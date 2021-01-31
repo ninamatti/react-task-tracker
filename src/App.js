eceb1cac533b0f4c76bca4cd5dbe18beea032111
Nina Matti <ninadesireematti@gmail.com>
2021-01-31 16:53:47 +0100

    Removed and changed unnecessary code bits

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-
+import Header from './components/Header';
 
 
 function App() {
   
   return (
     <div className="container">
-      <h1>Hello From Reahactu</h1>
-      
+      <Header />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types'
+
+
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1 style={headingStyle}>{title}</h1>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker'
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+// CSS in JS
+// const headingStyle = {
+//     color: 'green'
+// }
+
+export default Header
