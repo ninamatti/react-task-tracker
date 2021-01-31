fa11cf238eb8f4afb392e4dad67ea03d81196a3c
Nina Matti <ninadesireematti@gmail.com>
2021-01-31 21:59:59 +0100

    Adjusted App structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import Header from './components/Header';
+import Tasks from './components/Tasks';
+import { useState } from 'react'
 
 
 function App() {
-  
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id: 1,
+            text: 'Doc appointment',
+            day: 'Feb 5th at 2:30pm',
+            reminder: true,
+        },
+        {
+            id: 2,
+            text: 'Meeting at school',
+            day: 'Feb 6th at 1:30pm',
+            reminder: true,
+        },
+        {
+            id: 3,
+            text: 'Food shopping',
+            day: 'Feb 5th at 2:30pm',
+            reminder: false,
+        },
+    ]
+  )
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,17 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 
 const Header = ({title}) => {
+    const onClick = (e) => {
+       console.log('click') 
+    }
+
     return (
-        <header>
-            <h1 style={headingStyle}>{title}</h1>
+        <header className='header'>
+            <h1>{title}</h1> 
+            <Button color='green' text='Add' onClick={onClick} />
+            
         </header>
     )
 }
