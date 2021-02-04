cd86c657ea7a8445929bd6d8d9e8fcd5b68543a0
Nina Matti <ninadesireematti@gmail.com>
2021-02-04 20:23:55 +0100

    Added react-router-dom and created routes on app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
+import About from './components/About';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 
@@ -50,17 +53,31 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)}
-        showAdd={showAddTask}
-      />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? 
-        <Tasks tasks={tasks} onDelete={deleteTask} 
-          onToggle={toggleReminder} 
-        /> : 'No Tasks to show'
-        }
-    </div>
+    <Router>
+      <div className="container">
+        <Header onAdd={() => setShowAddTask(!showAddTask)}
+          showAdd={showAddTask}
+        />
+        
+        <Route path='/' exact render={(props) => (
+          <>
+            {showAddTask && <AddTask onAdd={addTask}/>}
+            {tasks.length > 0 ? (
+              <Tasks 
+                tasks={tasks} 
+                onDelete={deleteTask} 
+                onToggle={toggleReminder} 
+              />
+               ) : ( 
+                 'No Tasks to show'
+               )}
+          </>
+        )}/>
+        <Route path='/about' component={About} />
+        <Footer />
+      </div>
+    </Router>
+    
   );
 }
 
